892651b340268c916b2851910ce9b8b6b49ae5f4
Rafael Campanin Carneiro <rafaelcampanincarneiro@gmail.com>
2025-09-15 11:01:44 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarNome() {
+    let nome = document.getElementById("nome").value
+    if(nome != ""){
+        amigos.push(nome)
+        document.getElementById("nome").value = ""
+    }
+    else{
+        alert("Por favor, insira um nome!")
+    }
+    
+    let campo = document.querySelector("input");
+	campo.value = "";
+    atualizarLista();
+}
+
+function atualizarLista() {
+    limparCampo("listaAmigos");
+    let lista = document.getElementById("listaAmigos");
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    let sorteado;
+    limparCampo("listaAmigos");
+    limparCampo("resultado");
+    if (amigos.length >= 2) {
+        sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        let resultado = document.getElementById("resultado");
+        let item = document.createElement("li");
+        item.textContent = "O amigo secreto sorteado é: "+sorteado;
+        resultado.appendChild(item);
+    }
+    else {
+        alert("Adicione pelo menos dois amigos para sortear!");
+    }
+}
+
+function limparCampo(id) {
+    campo = document.getElementById(id);
+    campo.innerHTML = "";
+}
